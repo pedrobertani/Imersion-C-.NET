81a53d87a105d4162ebbc71cf47e587573965f26
Pedro Bertani <pedro.bertani@fatec.sp.gov.br>
2021-10-21 11:00:37 -0300

    Add project files.

---
+++ Estutos de C/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Estutos_de_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
