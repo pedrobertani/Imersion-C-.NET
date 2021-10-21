38ccfedea1646ae7025f3db2a2f0fae05d42d71f
Pedro Bertani <pedro.bertani@fatec.sp.gov.br>
2021-10-21 14:00:24 -0300

    Descreveno como utilizar e as boas praticas de utilizar variavel

--- Declarando variaveis.cs
+++ Declarando variaveis.cs
@@ -1,8 +1,39 @@
-﻿using System;
+﻿//Para criar uma variavel primeiro devemos declarar o tipo dela
+string primeiroNome;
+//Criamos uma variavel do tipo string 
+//De agora em diante essa variavel só pode receber valores em caracteres.
 
-public class Class1
-{
-	public Class1()
-	{
-	}
-}
+//Regras
+//O código de váriavel além de seguir algumas regras, precisa ser o mais legível possivel
+//afim de ter um código limpo e autoexplicativo
+//eles podem conter alfanumericos e alguns caracter especiais  _ # $ 
+//devem começar com uma letra alfabetica 
+//os desenvolvedores utilizam o caracter sublinhado para começar uma variavel com finalidade especial _APEAS_ESPECIAIS
+//nomes de variaveis não podem ser palavra-chave do C#, por exemplo string string ou int int
+//nomes de variaveis indentificam letras maiusculas e minusculas
+//string Value é uma variavel 
+//string value é outra variavel
+//Os nomes de variáveis devem usar minúsculas concatenadas, a uma letra minúscula
+//no início da primeira palavra e uma letra maiúscula no início de cada palavra subsequente.
+//Por exemplo, string aquiEstaUmExemplo;
+//Os nomes de variáveis devem ser uma ou mais palavras inteiras unidas.
+//Nomes de variáveis não devem incluir o tipo de dados da variável.
+//Você poderá ser aconselhado a usar um estilo como string strValue;. Esse conselho não é mais atual.
+//nosso exemplo string primeiroNome; segue todas as regras e convenções
+
+//tipos de variaveis usando a ideia de variavel e nome
+char userOption; //Opções de usuario podendo conter apenas um unico caracter.
+
+int gameScore; //Pontuação de jogo utilizando números
+
+decimal particlesPerMillion; //particulas decimais 
+
+bool processedCustomer; //verificando status de cliente, verdadeiro ou falso
+
+
+//Resumo
+//Variáveis são valores temporários que você armazena na memória do computador.
+//Para poder usar uma variável, você precisa declará-la.
+//Para declarar uma variável, você primeiro seleciona o tipo de dados que deseja armazenar e
+//em seguida, dá à variável um nome que siga as regras.
+
---
+++ Resumo váriaveis.cs
@@ -0,0 +1,5 @@
+﻿//Resumo
+//Variáveis são valores temporários que você armazena na memória do computador.
+//Para poder usar uma variável, você precisa declará-la.
+//Para declarar uma variável, você primeiro seleciona o tipo de dados que deseja armazenar e
+//em seguida, dá à variável um nome que siga as regras.
