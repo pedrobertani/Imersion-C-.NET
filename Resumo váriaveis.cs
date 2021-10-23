ac94fe09167c2b0a4219f62c21cbcbb2ff3faa13
pedrobertani <83377120+pedrobertani@users.noreply.github.com>
2021-10-23 10:25:29 -0300

    Delete Resumo váriaveis.cs

--- Resumo váriaveis.cs
+++
@@ -1,5 +0,0 @@
-﻿//Resumo
-//Variáveis são valores temporários que você armazena na memória do computador.
-//Para poder usar uma variável, você precisa declará-la.
-//Para declarar uma variável, você primeiro seleciona o tipo de dados que deseja armazenar e
-//em seguida, dá à variável um nome que siga as regras.
