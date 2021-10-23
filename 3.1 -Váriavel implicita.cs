79eae747845f8f97ee4a8f53db99a07db984bcdf
pedrobertani <pedro.bertani@fatec.sp.gov.br>
2021-10-23 10:20:11 -0300

    Atualizando

--- 3.1 -Váriavel implicita.cs
+++ 3.1 -Váriavel implicita.cs
@@ -1,10 +1,5 @@
 ﻿//Variavel Implicita
 
-//Uma variável local de tipo implícito é criada usando a palavra-chave var
-//que instrui o compilador de C# a inferir o tipo.
-//Depois de inferir o tipo, é como se o tipo de dados real tivesse sido usado para declarar a variável.
-//Ou seja, ela identifica no modo codado qual tipo de variavel é.
-
 var menssagem = "Hello World!";
 
 //O copilador entende que a variavel "menssagem" é do tipo string, pois são caracteres em aspas duplas " ... "
